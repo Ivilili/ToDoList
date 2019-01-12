c858c6fe1c3ffe516c40d3db2132a9adb92e85d5
Ivana Livaja <ivana.livaja@hotmail.com>
2019-01-12 22:38:03 +0100

    styling

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,20 +1,17 @@
 import React, { Component } from 'react';
+import { Input } from 'reactstrap';
 
 class Form extends Component {
-
-    render(){
-        return (
-            <div className="Form">
-              <form onSubmit={(evt) => this.props.handleSubmit(evt)}> 
-                  <input 
-                  onChange={(evt) => this.props.handleChange(evt)}
-                  value={this.props.inputValue}
-                  />
-              </form>
-            </div>
-        )
-    }
-
+	render() {
+		return (
+			<div className="Form">
+				<h1 className="display-2 text-center">ToDo List</h1>
+				<form onSubmit={(evt) => this.props.handleSubmit(evt)}>
+					<Input onChange={(evt) => this.props.handleChange(evt)} value={this.props.inputValue} />
+				</form>
+			</div>
+		);
+	}
 }
 
 export default Form;
--- src/components/List.js
+++ src/components/List.js
@@ -2,28 +2,17 @@ import React, { Component } from 'react';
 import Task from './Task';
 
 class List extends Component {
+	state = {};
 
-    state = {
-       
-    }
-
-    render() {
-        return (
-            <div className="List">
-         {this.props.todos.map((todo, index) => {
-                return (
-                    <Task 
-                    key={index} 
-                    index={index}
-                    handleClick={this.props.handleClick}
-                    todo={todo}
-                    /> 
-                )
-            })}
-            </div>
-        )
-    }
-
+	render() {
+		return (
+			<div className="List">
+				{this.props.todos.map((todo, index) => {
+					return <Task key={index} index={index} handleClick={this.props.handleClick} todo={todo} />;
+				})}
+			</div>
+		);
+	}
 }
 
 export default List;
